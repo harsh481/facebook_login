c6d0070679b51809f31a8c3e97c07a2214b3f6a4
harsh481 <harsh481@gmail.com>
2018-09-03 00:47:27 +0530

    Adding location

--- app/src/main/java/com/example/harsh/login/MainActivity.java
+++ app/src/main/java/com/example/harsh/login/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.harsh.login;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -22,6 +31,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,22 +41,45 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
 
     CallbackManager callbackManager;
-    TextView txtEmail, txtBirthday, txtFriends;
+    TextView textView, txtEmail, txtBirthday, txtFriends;
     ProgressDialog mDialog;
     ImageView imgAvatar;
+    LocationManager locationManager;
+    String provider;
+    double lat, lng;
+    final int My_PERMISSION_REQUEST_CODE = 7171;
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case My_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    getLocation();
+                break;
+
+        }
+    }
     //Press Ctrl+O
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode,resultCode,data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -55,16 +88,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        textView = (TextView) findViewById(R.id.txtAddress);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, My_PERMISSION_REQUEST_CODE);
+        } else {
+            getLocation();
+        }
+
+        //btnShow.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+                //if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                   // return;
+                //}
+                //Location myLocation = locationManager.getLastKnownLocation(provider);
+                //lat = myLocation.getLatitude();
+                //lng = myLocation.getLongitude();
+                //new GetAddress().execute(String.format("%.4f,%.4f",lat,lng));
+
+            //}
+        //});
+
         callbackManager = CallbackManager.Factory.create();
 
-        txtBirthday = (TextView)findViewById(R.id.txtBirthday);
-        txtEmail = (TextView)findViewById(R.id.txtEmail);
-        txtFriends = (TextView)findViewById(R.id.txtFriends);
+        txtBirthday = (TextView) findViewById(R.id.txtBirthday);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtFriends = (TextView) findViewById(R.id.txtFriends);
 
-        imgAvatar = (ImageView)findViewById(R.id.avatar);
+        imgAvatar = (ImageView) findViewById(R.id.avatar);
 
-        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList("public_profile","email","user_birthday","user_friends"));
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -73,8 +133,8 @@ public class MainActivity extends AppCompatActivity {
                 mDialog.setMessage("Retriving data...");
                 mDialog.show();
 
-                //if alreasy login
-                if(AccessToken.getCurrentAccessToken()!=null);
+                //if already login
+                if (AccessToken.getCurrentAccessToken() != null) ;
                 {
                     //justsetuserid
                     txtEmail.setText(AccessToken.getCurrentAccessToken().getUserId());
@@ -94,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                 //Request Graph API
 
                 Bundle parameters = new Bundle();
-                parameters.putString("fields","id,email,birthday,friends");
+                parameters.putString("fields", "id,email,birthday,friends");
                 request.setParameters(parameters);
                 request.executeAsync();
 
@@ -112,6 +172,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         printKeyHash();
+    }
+
+    private void getLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        final Location location = locationManager.getLastKnownLocation(provider);
+        if(location == null)
+            Log.e("Error","location is null");
     }
 
     private void getData(JSONObject object) {
@@ -150,6 +224,75 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+
+        new GetAddress().execute(String.format("%.4f,%.4f",lat,lng));
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private class GetAddress extends AsyncTask<String,Void,String> {
+
+        ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please Wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                double lat = Double.parseDouble(strings[0].split(",")[0]);
+                double lng = Double.parseDouble(strings[0].split(",")[1]);
+                String response;
+                HttpDataHandler http = new HttpDataHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?latlng=%.4f,%.4f&sensor=false",lat,lng);
+                response = http.GetHTTPData(url);
+                return response;
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try{
+                JSONObject jsonObject = new JSONObject(s);
+                String address = ((JSONArray)jsonObject.get("results")).getJSONObject(0).get("formatted_addess").toString();
+                textView.setText(address);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if(dialog.isShowing())
+                dialog.dismiss();
         }
     }
 }
